8fa846dd888e114558ac6a8b0871185fd7b2783f
Yashagarwallaa <yashagarwalla001@gmail.com>
2025-03-06 21:53:00 +0530

    modified home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,9 +13,9 @@ const Home = () => {
       width={1000}
       height={50}/> */}
       <div className='flex h-full flex-col justify-between max-md:px-2 max-md:py-8 lg:p-7'>
-      <h2 className='bg-white bg-opacity-10 text-center max-w-[270px] rounded py-2 px-1 max-sm:px-2 text-base font-normal'>
+      {/* <h2 className='bg-white bg-opacity-10 text-center max-w-[270px] rounded py-2 px-1 max-sm:px-2 text-base font-normal'>
         Upcoming meeting at 12:30 PM
-        </h2>
+        </h2> */}
         <div className='flex flex-col gap-2'>
           <h1 className='text-4xl font-extrabold lg:text-6xl max-sm:px-2'>{time}</h1>
           <p className=' text-lg px-1 font-bold text-sky-1 lg:text-1xl max-sm:px-2'>{date}</p>
